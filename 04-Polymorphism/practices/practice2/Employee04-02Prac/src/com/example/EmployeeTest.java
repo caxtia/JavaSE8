270df2674c5f7281613c895be50f98b867d69eb7
Catia Lopes <csousalopes@hotmail.com>
2021-01-12 12:07:18 +0000

    exercicio 4.2

--- 04-Polymorphism/practices/practice2/Employee04-02Prac/src/com/example/EmployeeTest.java
+++ 04-Polymorphism/practices/practice2/Employee04-02Prac/src/com/example/EmployeeTest.java
@@ -11,18 +11,19 @@ public class EmployeeTest {
 
         // Create the classes as per the practice
         Engineer eng = new Engineer(101, "Jane Smith", "012-34-5678", 120_345.27);
-        //Employee emp = new Employee(13, "Lionel Power", "099-90-6789", 67_990.90);
+        Employee emp = new Employee(13, "Lionel Power", "099-90-6789", 67_990.90);
         Employee obj = new Engineer(102, "Robert Stock", "012-54-7812", 220_345.27);
 
-        eng.engineerMethod();
+        if(obj instanceof Engineer){
+            ((Engineer)obj).engineerMethod();	    
+        }
+        
         printEmployee(obj);
 
         Engineer engobj =  new Engineer(1, "Brenda Wills", "013-78-5678", 221_500.00);
         printEmployee(engobj);
 
-        //String s = (String) emp.getName();
-        //System.out.println(s);
-
+        String s = " " + emp;
     }
 
     
