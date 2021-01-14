820657aed3809f364e18d68794b63769fc2aa46a
Catia Lopes <csousalopes@hotmail.com>
2021-01-14 11:42:15 +0000

    exeercicio 7

--- 07-Generics_Collections/practices/practice1/GenericsHashMap07-01Prac/src/com/example/generics/ProductCounter.java
+++ 07-Generics_Collections/practices/practice1/GenericsHashMap07-01Prac/src/com/example/generics/ProductCounter.java
@@ -3,15 +3,20 @@ package com.example.generics;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProductCounter {
     // Create a Counting Map
     // Create a Name Mapping Map
+    private Map<String, Long> partNumbers = new ConcurrentHashMap<>();
+    private Map<String, String> descriptProduct = new TreeMap<>();
 
     public static void main(String[] args) {
 
         // List of part data
-        String[] parts = new String[]{"1S01", "1S01", "1S01", "1S01", "1S01", "1S02", "1S02", "1S02", "1H01", "1H01", "1S02", "1S01", "1S01", "1H01", "1H01", "1H01", "1S02", "1S02", "1M02", "1M02", "1M02"};
+        String[] parts = new String[] { "1S01", "1S01", "1S01", "1S01", "1S01", "1S02", "1S02", "1S02", "1H01", "1H01",
+                "1S02", "1S01", "1S01", "1H01", "1H01", "1H01", "1S02", "1S02", "1M02", "1M02", "1M02" };
 
         // Create Product Name Part Number map
         Map<String, String> productNames = new TreeMap<>();
@@ -21,17 +26,36 @@ public class ProductCounter {
         productNames.put("Duke Mug   ", "1M02");
 
         // Create Product Counter Object and process data
+        ProductCounter product = new ProductCounter((productNames));
+        product.processList(parts);
+        product.printReport();
+
     }
 
-    public ProductCounter(Map productNames) {
+    public ProductCounter(Map<String, String> productNames) {
         // Your code here
+        for (Entry<String, String> entry : productNames.entrySet()) {
+            descriptProduct.put(entry.getValue(), entry.getKey());
+        }
+       
     }
 
     public void processList(String[] list) {
         // your code here
+        for (String string : list) {
+            if(partNumbers.containsKey(string)){
+                Long count = partNumbers.get(string);
+                count ++;
+                partNumbers.put(string, count);
+            }else {
+                partNumbers.put(string, 1l);
+            }
+        }
     }
 
     public void printReport() {
         // Your code here
+        System.out.println(partNumbers);
+        System.out.println(descriptProduct);
     }
 }
