bdac3845b82efe89d34e15788ab5bfe300f3d909
Catia Lopes <csousalopes@hotmail.com>
2021-01-17 16:30:10 +0000

    Exercicio 10-1

--- 10-LambdaOperations/practices/practice1/SalesTxn10-01Prac/src/com/example/lambda/PeekTest.java
+++ 10-LambdaOperations/practices/practice1/SalesTxn10-01Prac/src/com/example/lambda/PeekTest.java
@@ -2,6 +2,7 @@ package com.example.lambda;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -17,9 +18,20 @@ public class PeekTest {
         + " Buyer: " + t.getBuyerName() + 
         " Txn amt: $%,9.2f ", t.getTransactionTotal());
     
+        
     // Write report using peek
     System.out.println("=== Widget Pro Sales Tax in CA ===");
-
+   
+    tList.stream()
+            
+            .filter(t->t.getState().equals(State.CA))
+            .filter(t->t.getProduct().equals("Widget Pro"))
+            .peek(t -> System.out.printf("ID: " + t.getTxnId() + " Buyer: " + t.getBuyerName() + " TXN AMt: " + t.getTransactionTotal()))
+            .map(t->t.getTransactionTotal() * TaxRate.byState(t.getState()))
+            .peek(t -> System.out.printf(" Txn tax: $%,9.2f%n", t))
+       
+            .collect(Collectors.toList());
+ 
         
         
     }
