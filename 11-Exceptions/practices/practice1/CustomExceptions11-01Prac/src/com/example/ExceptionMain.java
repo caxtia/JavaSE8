2db0b40e5fe623bf26f0b7ec342cf32f4423e215
Catia Lopes <csousalopes@hotmail.com>
2021-01-18 10:55:15 +0000

    exercicio 11-1

--- 11-Exceptions/practices/practice1/CustomExceptions11-01Prac/src/com/example/ExceptionMain.java
+++ 11-Exceptions/practices/practice1/CustomExceptions11-01Prac/src/com/example/ExceptionMain.java
@@ -10,26 +10,38 @@ public class ExceptionMain {
     public static void main(String[] args) throws FileNotFoundException {
 
         try {
-            // System.out.println("Reading from file:" + args[0]);
-            BufferedReader b = new BufferedReader(new FileReader(args[0]));
-            String s = null;
-
-            while ((s = b.readLine()) != null) {
-                System.out.println(s);
-            }
-
+             System.out.println("Reading from file:" + args[0]);
+        
             
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("No file specified, quitting!");
             System.exit(1);
 
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            System.out.println("Error: " + e.getMessage());
-            e.printStackTrace();
-            System.exit(1);
-            
-        }
+        } 
         
+        try (
+          
+            BufferedReader b = new BufferedReader(new FileReader(args[0]));
+        ){
+            String s = null;
+
+            while ((s = b.readLine()) != null) {
+                System.out.println(s);
+            }
+        }
+        catch(FileNotFoundException notFoundException){
+            assert args.length >0;
+            System.err.println("Couldn't find file"+ notFoundException.getMessage());
+
+        }
+        catch (IOException e) {
+                // TODO Auto-generated catch block
+                System.out.println("Error reading file: " + e.getMessage());
+                e.printStackTrace();
+                System.exit(1);
+                
+            }
+
+
     }
 }
