a5205f5ea215552da7e984a40686c73861af6e61
Catia Lopes <csousalopes@hotmail.com>
2021-01-19 09:25:46 +0000

    exercicio 13-1

--- 13-IO_Fundamentals/practices/practice1/FileScanner13-01Prac/src/com/example/FileScanInteractive.java
+++ 13-IO_Fundamentals/practices/practice1/FileScanner13-01Prac/src/com/example/FileScanInteractive.java
@@ -3,6 +3,7 @@ package com.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class FileScanInteractive {
@@ -24,5 +25,35 @@ public class FileScanInteractive {
 
     public static void main(String[] args) {
         // Your code goes here
+        if (args.length < 1) {
+            System.out.println("Args less than one");
+            System.exit(-1);
+        }
+
+        String firstArg = args[0];
+        FileScanInteractive scan = new FileScanInteractive ();
+  
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String search = "";
+            System.out.println ("Local File: " + firstArg);
+            
+            while (true) {
+                System.out.print("Enter the search string or q to exit: ");
+                search = reader.readLine().trim();
+                if (search.equalsIgnoreCase("q")) break;
+                  
+
+            int count = scan.countTokens(firstArg, search);
+            System.out.println("The word \"" + search + "\" appears " + count + " times in the file");
+            }
+
+        } catch (IOException e) {
+             System.out.println("Exception: " + e);
+             System.exit(-1);
+        }
+        
+        
+
+       
     }
 }
