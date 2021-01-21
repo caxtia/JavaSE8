594414b93a46665c6aa56b66a71f7ee30ac90727
Catia Lopes <csousalopes@hotmail.com>
2021-01-21 09:11:38 +0000

    exercicio 17-03

--- 17-ParallelStreams/practices/practice3/SalesTxn17-03Prac/src/com/example/lambda/CalcTest.java
+++ 17-ParallelStreams/practices/practice3/SalesTxn17-03Prac/src/com/example/lambda/CalcTest.java
@@ -14,12 +14,22 @@ public class CalcTest {
         
         // Print out Transaction Totals
         System.out.println("=== Transactions Totals ===");
-       
-        double t2 = 0; //  PriceCo code here
+        double t2 = tList.stream()
+            .filter(t -> t.getBuyerName().equals("PriceCo"))
+            .mapToDouble( t -> t.getTransactionTotal())
+            .parallel()
+            .reduce(0, (sum, e) -> sum + e);
+ 
+        //double t2 = 0; //  PriceCo code here
         
         System.out.printf("%nPriceCo Total: $%,9.2f%n", t2);
 
-        int c2 = 0; // PriceCo count code here
+        //int c2 = 0; // PriceCo count code here
+        int c2 = tList.stream()
+            .filter(t -> t.getBuyerName().equals("PriceCo"))
+            .mapToInt(t -> 1)
+            .parallel()
+            .reduce(0, (sum, e) -> sum + e);
         
         System.out.printf("PriceCo Transactions: %,6d%n", c2);
         
